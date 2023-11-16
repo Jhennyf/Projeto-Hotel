6e2024537d5f2178c1c5397801c3c2bac63bf3ab
Jhennyf <jhennyfestudos2021@gmail.com>
2023-11-16 00:09:18 -0300

    versao atualizada

---
+++ src/components/carrossel/carrosel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-carrosel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carrosel.component.html',
+  styleUrl: './carrosel.component.css'
+})
+export class CarroselComponent {
+
+}
--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarroselComponent } from '../carrossel/carrosel.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CarroselComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
